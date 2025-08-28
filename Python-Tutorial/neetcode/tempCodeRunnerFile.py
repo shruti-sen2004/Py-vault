76c683120db62aba5d6ba6bfc31882124c252fdb
Shruti Sen <senshruti2020@gmail.com>
2025-08-28 21:49:50 +0530

    Add code snipet for sorting Dictionary on the basis of values

--- Python-Tutorial/neetcode/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-my_dict.pop("b")
-# print(my_dict)
-# #my_dict.pop("d") # raises Keyerror
-# my_dict.pop("d",0) # handle it; don't throw error just return 0
-
-# for value in my_dict.values(): # iterate ver values
-#     print(value)
